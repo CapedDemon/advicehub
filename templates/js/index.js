c5fa46cbf110d12c824430bc98ed7988525c02dd
Shreejan <dolaishreejan@gmail.com>
2022-09-16 13:33:01 +0530

    Improved js and responsive

--- templates/js/index.js
+++ templates/js/index.js
@@ -1,6 +1,6 @@
 function httpGet(theUrl) {
   let xmlHttpReq = new XMLHttpRequest();
-  xmlHttpReq.open("GET", theUrl, false); 
+  xmlHttpReq.open("GET", theUrl, false);
   xmlHttpReq.send(null);
   return xmlHttpReq.responseText;
 }
@@ -9,16 +9,16 @@ const adviceBTN = document.getElementById("adviceBTN");
 const secondb = document.getElementById("secondb");
 
 adviceBTN.addEventListener("click", () => {
-    var adviceObj = httpGet('http://advicehub.onrender.com/suradvice');
-    var parsed = JSON.parse(adviceObj);
+  var adviceObj = httpGet("https://advicehub.onrender.com/suradvice");
+  var parsed = JSON.parse(adviceObj);
 
-    var description = parsed.data.description;
-    var author = parsed.data.author;
+  var description = parsed.data.description;
+  var author = parsed.data.author;
 
-    console.log(author)
+  console.log(author);
 
-    secondb.innerHTML = `<p class="secondp">" ${description} "<br>- ${author}</p>`
-    secondb.style.color = "white"
-    secondb.style.fontFamily = "'Ms Madi', cursive";
-    secondb.style.fontSize = "24px"
+  secondb.innerHTML = `<p class="secondp">" ${description} "<br>- ${author}</p>`;
+  secondb.style.color = "white";
+  secondb.style.fontFamily = "'Ms Madi', cursive";
+  secondb.style.fontSize = "24px";
 });
